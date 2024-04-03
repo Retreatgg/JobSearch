88122141cddcb3a921f724aac89d11758f738d99
Argen Ashimov <retreatpm@icloud.com>
2024-04-03 20:23:57 +0600

    Add edit profile

--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -3,26 +3,33 @@ package com.example.demo.controller;
 import com.example.demo.dto.UserUpdateDto;
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("profile")
 public class ProfileController {
     private final UserService userService;
-    @PostMapping("{id}")
-    public HttpStatus editProfile(@PathVariable long id, Authentication authentication, UserUpdateDto updateDto) {
+    @PostMapping("edit")
+    public String editProfile(Authentication authentication, UserUpdateDto updateDto) {
         userService.editProfile(updateDto, authentication);
-        return HttpStatus.OK;
+        return "redirect:/";
+    }
+
+    @GetMapping("edit")
+    public String editProfile(Model model){
+        model.addAttribute("user", userService.getUserByEmail("bob@example.com"));
+        return "profile/edit_profile";
     }
 
     @GetMapping
     public String getProfile(Authentication authentication, Model model) {
-        model.addAttribute("user", userService.getUserByEmail("john@example.com"));
+        model.addAttribute("user", userService.getUserByEmail("bob@example.com"));
         return "profile/profile";
     }
 }
